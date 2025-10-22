54e43342f165f68d6958e1a0b9aca53b1c170356
s+Sveltduck <tommfgibbs@gmail.com>
2025-10-22 13:18:32 +0100

    Added Range, id, Habits, sharing, and interst classes

--- basic-matcher.py
+++ basic-matcher.py
@@ -1,3 +1,78 @@
+class Range:
+    """
+    Takes the integer values between between 1 and 5 
+    
+    
+    """
+
+    def __init__(self,value):
+        self.value= min(abs(value),5)
+        
+
+
+
+class Id:
+    """
+    The aim of the id class is to allow scllearer acces to a given student's identificatory predicates 
+    Attributes:
+    preferredName: A person's preffered name
+    idNumber: the student id number
+    gender: the students gender (for the purposes of roomsharing)
+    allergies: the student's allergies
+    """
+    def __init__(self,preferredName,idNumber,gender,courseCode,allergies):
+        pass
+
+class Habits:
+    """
+    Denotes general habits 
+    Attributes:
+    roomSharing: whether or not the student wants to share a room 
+    regularSleet: times at which the student usually sleeps
+    exceptionalSleep: sleep times during weekends or nights out
+    tidyness: how tidy someone is
+    sociability: how tidy somone is
+    """
+    def __init__(self, roomSharing,
+                 regularSleep, exceptionalSleep, tidyness,sociability ):
+        pass
+class Sharing:
+    """
+    Indicates attitude to sharing a space 
+    Attributes:
+    othersDaytimeVisitors: how they feel about a roomate briging others round during the day
+    othersNighttimeVisitors: how they feel about a roomate briging others round at nigh/evening, potetially to sleep
+    daytimeVisitors: how likely soneone is to invite people round dur ing the day
+    nighttimeVisitors: how likely somone is to invite people round in the evening/ at night
+
+
+    """
+
+
+    def __init__(self, othersDaytimeVisitors,othersNighttimeVisitors,daytimeVisitors, nightimeVisitors ):
+        pass
+
+class Interests:
+    """
+    Provides a persons's interests
+    Arrributes: 
+    sports 
+    
+    
+    """ 
+
+    def __init__(self, sports, politics, films,filmGenres, music,musicGenres ):
+        pass
+
 class Student:
-    def __init__(self):
+    def __init__(self,id,habits, sharing,interests):
+        self.id=id
+        self.habits=habits
+        self.sharing=sharing
+        self.interests=interests
+                 
+                 
+                 
+                 
+                
         return True
